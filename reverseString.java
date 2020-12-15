0970d9d4ffd746c3c8658325846a3e043ffd409c
han0305 <roy.aditya1605@gmail.com>
2020-12-15 22:07:20 +0530

    96th day of endless coding

---
+++ reverseString.java
@@ -0,0 +1,19 @@
+/* Write a function that reverses a string. The input string is given as an array of characters char[].
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+You may assume all the characters consist of printable ascii characters. */
+
+class Solution {
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+        while(start<end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
