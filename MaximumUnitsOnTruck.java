5bd2ba5bab3bf0d361628a463b3fb313f3d518ae
han0305 <roy.aditya1605@gmail.com>
2021-01-22 23:54:01 +0530

    134th day of endless coding

---
+++ MaximumUnitsOnTruck.java
@@ -0,0 +1,31 @@
+/* You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck. You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck. */
+
+class CustomSort implements Comparator<int[]> {
+    
+    public int compare(int[] a,int[] b) {
+        
+        return b[1]-a[1];
+    }
+    
+}
+
+class Solution {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        
+        Arrays.sort(boxTypes,new CustomSort());
+        int total = 0;
+        for(int i=0;i<boxTypes.length && truckSize!=0;i++) {
+            int boxes  = Math.min(truckSize,boxTypes[i][0]);
+            total+=boxes*boxTypes[i][1];
+            truckSize-=boxes;
+        }
+        
+        return total;
+    }
+}
