8dad74f02a0aa42d57862187a00bcf3feabdaaed
han0305 <roy.aditya1605@gmail.com>
2021-03-22 22:27:21 +0530

    185th day of endless coding

---
+++ MaximumAscendingSubArraySum.java
@@ -0,0 +1,25 @@
+/* Given an array of positive integers nums, return the maximum possible sum of an ascending subarray in nums.
+
+A subarray is defined as a contiguous sequence of numbers in an array.
+
+A subarray [numsl, numsl+1, ..., numsr-1, numsr] is ascending if for all i where l <= i < r, numsi < numsi+1. Note that a subarray of size 1 is ascending. */
+
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        
+        int maxm = nums[0];
+        int sum = nums[0];
+        
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i]>nums[i-1]) {
+                sum+=nums[i];
+            }
+            else {
+                sum=nums[i];
+            }
+            maxm = Math.max(maxm,sum);
+        }
+        
+        return maxm;
+    }
+}
